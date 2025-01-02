02f4fa6253cd14b43593ef6ede11dd2f9e029af9
conormvickers <conormvickers@gmail.com>
2025-01-02 23:59:07 +0000

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.discoverderm',
+  appName: 'docdocgoose',
+  webDir: 'public'
+};
+
+export default config;
