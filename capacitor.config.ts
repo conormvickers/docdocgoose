0ee200be9aed21b416ac6a4ce372b78e569e352f
conormvicker <conormvickers@gmail.com>
2025-01-14 19:31:01 -0500

    resetup fastlane

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.discoverderm',
+  appName: 'docdocgoose',
+  webDir: 'dist'
+};
+
+export default config;
