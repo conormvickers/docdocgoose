92075f378dd3bfd84e6a79a35bc97332e8027cc6
conormvickers <conormvickers@gmail.com>
2025-01-15 00:05:31 +0000

    switched to vite

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from "@capacitor/cli";
-
-const config: CapacitorConfig = {
-  appId: "com.discoverderm",
-  appName: "docdocgoose",
-  webDir: "out",
-};
-
-export default config;
