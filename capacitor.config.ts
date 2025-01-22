01a14cb5282b02cc332abe88fbeeddced60a6ad8
conormvickers <conormvickers@gmail.com>
2025-01-22 17:12:39 +0000

    ok trying config changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.discoverderm',
-  appName: 'docdocgoose',
-  webDir: 'dist'
+  appId: "com.discoverderm",
+  appName: "docdocgoose",
+  webDir: "dist",
+  ios: {
+    contentInset: "always",
+  },
 };
 
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import PBprovider from "./PBProvider/provider";
-import "@ionic/react/css/core.css";
-import { setupIonicReact } from "@ionic/react";
-import { IonHeader, IonTitle, IonToolbar } from "@ionic/react";
 
-setupIonicReact();
 function App() {
   return (
     <div>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Header</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <PBprovider />
     </div>
   );
